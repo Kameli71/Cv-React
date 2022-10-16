4c05e6b08f068c64bff0b2e010e274ed708532f8
Kameli71 <karim.kameli71@gmail.com>
2022-10-16 23:22:09 +0200

    corrections

--- src/components/Profil.jsx
+++ src/components/Profil.jsx
@@ -5,8 +5,8 @@ function Profil() {
     return (
         <div className="profil mb5">
             <h2 className="h2">Profil</h2>
-            <p className="intro">J'ai décidé de m'engager une reconversion professionnelle afin de devenir Développeur web pour pouvoir exercer un métier qui me correspond et me passionne.</p>
-            <p className="intro">C'est pourquoi je suis à la recherche d'un emploi dans une entreprise qui me permettra de mettre en application mes acquis et de m'aider à me perfectionner dans l'univer du Dev.</p>
+            <p className="intro">J'ai décidé de m'engager dans une reconversion professionnelle afin de devenir Développeur web pour exercer un métier qui me correspond et me passionne.</p>
+            <p className="intro">C'est pourquoi je suis à la recherche d'un emploi dans une entreprise qui me permettra de mettre en application mes acquis et de m'aider à me perfectionner dans l'univers du Dev.</p>
         </div>
     )
 }
